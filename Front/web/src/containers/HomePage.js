97ad70e79ae407ee85f4015aaa8d01e816f3743b
Bastien Genestal <bastien.genestal@epitech.eu>
2020-04-24 18:48:04 +0200

    discord invite updated

--- Front/web/src/containers/HomePage.js
+++ Front/web/src/containers/HomePage.js
@@ -4,6 +4,8 @@ import discord from "../data/image/logos/JPG/discord.png";
 import { MDBMask, MDBView, MDBBtn } from 'mdbreact';
 import Header from "../components/Header/Header";
 
+const discord_link = "https://discord.gg/axhYkm4"
+
 class HomePage extends Component {
 
     render() {
@@ -14,7 +16,7 @@ class HomePage extends Component {
           <MDBView src={bgPic}>
               <MDBMask overlay="black-light" className="flex-center flex-column text-white text-center">
                   <h2 className="intro-text">Get into Darwin Duel League</h2>
-                  <MDBBtn outline color="light-blue" href="https://discord.gg/tNJXUca" target="_blank" size="lg" style={{display:'flex', padding: 0}}>
+                  <MDBBtn outline color="light-blue" href={discord_link} target="_blank" size="lg" style={{display:'flex', padding: 0}}>
                       <div className="discordButtonContent">
                           <img alt="discord" src={discord} width='50px'/>
                           <div style={{display: "table-cell", paddingLeft: "5px", margin: "auto", verticalAlign: 'middle'}} >Join Discord</div>
