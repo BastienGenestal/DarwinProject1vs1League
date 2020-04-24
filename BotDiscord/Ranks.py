c72a71715e88b06584a6eccf3f5d85d422bae012
Bastien Genestal <bastien.genestal@epitech.eu>
2020-04-24 22:43:33 +0200

    fixing rank ===

--- BotDiscord/Ranks.py
+++ BotDiscord/Ranks.py
@@ -20,7 +20,7 @@ async def set_rank(client, player, player_elo):
     player_elo = int(player_elo)
     new_rank = get_new_rank(player_elo)
     current_rank = get_rank(client, player)
-    if current_rank.id == new_rank.id:
+    if current_rank == new_rank:
         return
     await clear_rank(client, player)
     await player.add_roles(client.RankRoles[new_rank])
